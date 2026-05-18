530772d2f9ea3eeb2163e4f392d585da1ec1b9d1
Raúl Garcia <ego.war@icloud.com>
2026-05-18 05:49:14 -0700

    ✨ Vercel
    vercel server

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -3,7 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import sanity from '@sanity/astro';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), sanity({
+    projectId: 'u0y4587v',
+      dataset: 'production',
+      useCdn: false, // See note on using the CDN
+      apiVersion: "2026-05-25", // insert the current date to access the latest version of the API
+
+  })],
+  output: 'server',
+  adapter: vercel()
 });
